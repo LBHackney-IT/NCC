f3efd274313a038439fee25f1bcf3cc6b4423051
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-08-20 16:09:50 +0100

    Extend the original dialogue component.

--- ng-ncc-app/src/app/components/dialogue/confirm/confirm-dialogue.component.ts
+++ ng-ncc-app/src/app/components/dialogue/confirm/confirm-dialogue.component.ts
@@ -1,38 +1,28 @@
 import { Component, EventEmitter, HostListener, Input, Output, ElementRef, ViewChild, OnChanges, SimpleChange } from '@angular/core';
-
-// TODO we could probably extend the dialogue component to create this one.
+import { DialogueComponent } from '../dialogue.component';
 
 @Component({
     selector: 'app-confirm-dialogue',
     templateUrl: './confirm-dialogue.component.html',
     styleUrls: ['./confirm-dialogue.component.scss']
 })
-export class ConfirmDialogueComponent implements OnChanges {
-    @Input() show: boolean;
+export class ConfirmDialogueComponent extends DialogueComponent {
+
+    // This component extends the original dialogue component.
+
     @Input() yesButtonText: string;
     @Input() noButtonText: string;
-    @Output() showChange = new EventEmitter<boolean>();
     @Output() selectedYes = new EventEmitter<void>();
     @Output() selectedNo = new EventEmitter<void>();
 
     defaultYesButtonText = 'Yes';
     defaultNoButtonText = 'No';
 
-    // https://stackoverflow.com/a/46720813/4073160
-    @ViewChild('dialogueButton') dialogueButton: ElementRef;
-
     // Listen for the Esc key to close the dialogue.
     // It will only work if the dialogue within is in focus.
     @HostListener('keyup.esc', ['$event'])
     onListenerTriggered(event: UIEvent): void {
         this.answerNo();
-    }
-
-    ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
-        if (changes.show && true === changes.show.currentValue) {
-            // Set the focus on the button.
-            setTimeout(() => { this.dialogueButton.nativeElement.focus(); }, 1);
-        }
     }
 
     answerYes() {
