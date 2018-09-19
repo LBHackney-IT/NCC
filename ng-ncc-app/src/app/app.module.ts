e8613261425ba1a6f6cfb1b40ed4bf498fef5a51
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-09-19 14:12:40 +0100

    Created a base communications page component to address code duplication.

--- ng-ncc-app/src/app/app.module.ts
+++ ng-ncc-app/src/app/app.module.ts
@@ -48,6 +48,7 @@ import { HelperComponent } from './components/helper/helper.component';
 import { OrComponent } from './components/or/or.component';
 import { CommsTemplatesComponent } from './components/comms-templates/comms-templates.component';
 import { DPADialogueComponent } from './components/dialogue/dpa/dpa-dialogue.component';
+import { CommunicationsPageComponent } from './components/communications-page/communications-page.component';
 
 @NgModule({
     declarations: [
@@ -85,6 +86,7 @@ import { DPADialogueComponent } from './components/dialogue/dpa/dpa-dialogue.com
         OrComponent,
         CommsTemplatesComponent,
         DPADialogueComponent,
+        CommunicationsPageComponent,
     ],
     imports: [
         BrowserModule,
