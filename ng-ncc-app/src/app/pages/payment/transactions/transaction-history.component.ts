18efea14e3632a648c7c173fae65f8d383ac1c7e
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-09-27 15:06:48 +0100

    Addressing code duplication.

--- ng-ncc-app/src/app/pages/payment/transactions/transaction-history.component.ts
+++ ng-ncc-app/src/app/pages/payment/transactions/transaction-history.component.ts
@@ -37,18 +37,6 @@ export class PageTransactionHistoryComponent extends PageHistoryComponent implem
             manual: {}
         };
 
-        // Set up the period options.
-        const current_year = new Date().getFullYear();
-        const previous_year = current_year - 1;
-        const two_years_ago = current_year - 2;
-        this.period_options = [
-            { key: this.PERIOD_SIX_MONTHS, label: 'Last 6 months' },
-            { key: this.PERIOD_TWELVE_MONTHS, label: 'Last 12 months' },
-            { key: current_year.toString(), label: current_year.toString() },
-            { key: previous_year.toString(), label: previous_year.toString() },
-            { key: two_years_ago.toString(), label: two_years_ago.toString() }
-        ];
-
         this.route.data
             .subscribe((data) => {
                 this.account_details = data.accountDetails;
@@ -64,36 +52,6 @@ export class PageTransactionHistoryComponent extends PageHistoryComponent implem
         this.filter.manual = {
             debDesc: this.filter_type
         };
-    }
-
-    /**
-     * Set the date filtering option for the list of transactions.
-     */
-    filterByDate() {
-        let min_date = new Date();
-        let max_date = new Date();
-
-        switch (this.period) {
-            case this.PERIOD_SIX_MONTHS:
-                min_date.setMonth(min_date.getMonth() - 6);
-                break;
-
-            case this.PERIOD_TWELVE_MONTHS:
-                min_date.setFullYear(min_date.getFullYear() - 1);
-                break;
-
-            default:
-                // If the selected option is numeric, assume it is a specific year.
-                // Otherwise no date filter will be applied.
-                const year = parseInt(this.period, 10);
-                if (!Number.isNaN(year)) {
-                    min_date = new Date(year, 0, 1);
-                    max_date = new Date(year + 1, 0, 0);
-                }
-        }
-
-        this.filter.min_date = min_date;
-        this.filter.max_date = max_date;
     }
 
     /**
--- ng-ncc-app/src/app/pages/view-notes/view-notes.component.ts
+++ ng-ncc-app/src/app/pages/view-notes/view-notes.component.ts
@@ -40,18 +40,6 @@ export class PageViewNotesComponent extends PageHistoryComponent implements OnIn
         };
         this.clearFilter();
 
-        // Set up the period options.
-        const current_year = new Date().getFullYear();
-        const previous_year = current_year - 1;
-        const two_years_ago = current_year - 2;
-        this.period_options = [
-            { key: this.PERIOD_SIX_MONTHS, label: 'Last 6 months' },
-            { key: this.PERIOD_TWELVE_MONTHS, label: 'Last 12 months' },
-            { key: current_year.toString(), label: current_year.toString() },
-            { key: previous_year.toString(), label: previous_year.toString() },
-            { key: two_years_ago.toString(), label: two_years_ago.toString() }
-        ];
-
         this.route.data
             .subscribe((data) => {
                 this.caller = data.caller;
@@ -65,38 +53,8 @@ export class PageViewNotesComponent extends PageHistoryComponent implements OnIn
     filterTransactions() {
         this.filter.manual = {
             callReasonType: this.filter_reason,
-            crmContactID: this.filter_caller
+            contactId: this.filter_caller
         };
-    }
-
-    /**
-     * Set the date filtering option for the list of transactions.
-     */
-    filterByDate() {
-        let min_date = new Date();
-        let max_date = new Date();
-
-        switch (this.period) {
-            case this.PERIOD_SIX_MONTHS:
-                min_date.setMonth(min_date.getMonth() - 6);
-                break;
-
-            case this.PERIOD_TWELVE_MONTHS:
-                min_date.setFullYear(min_date.getFullYear() - 1);
-                break;
-
-            default:
-                // If the selected option is numeric, assume it is a specific year.
-                // Otherwise no date filter will be applied.
-                const year = parseInt(this.period, 10);
-                if (!Number.isNaN(year)) {
-                    min_date = new Date(year, 0, 1);
-                    max_date = new Date(year + 1, 0, 0);
-                }
-        }
-
-        this.filter.min_date = min_date;
-        this.filter.max_date = max_date;
     }
 
     /**
