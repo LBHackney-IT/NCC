0c589f00c7c7878efa24ca7cce2b3b28b29c7b89
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-10-02 14:49:09 +0100

    Added an end call link to the navigation. This is simply a demonstration of how we can "end" a call and start again.

--- ng-ncc-app/src/app/components/navigation/navigation.component.ts
+++ ng-ncc-app/src/app/components/navigation/navigation.component.ts
@@ -2,10 +2,24 @@
 // <app-navigation></app-navigation>
 
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { CallService } from '../../services/call.service';
 
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent { }
+export class NavigationComponent {
+
+    constructor(private Call: CallService, private router: Router) { }
+
+    /**
+     * "Ends" the current call and navigates to the log call page.
+     */
+    endCall() {
+        this.Call.reset();
+        this.router.navigateByUrl('/log-call');
+    }
+}
