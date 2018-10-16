2be8f5226b064d1991a1b28b5d717bd2ff359e30
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-10-16 13:46:42 +0100

    Fetch and update editorial text.

--- ng-ncc-app/src/app/components/editorial/editorial.component.ts
+++ ng-ncc-app/src/app/components/editorial/editorial.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { NCCAPIService } from '../../API/NCCAPI/ncc-api.service';
 
 @Component({
     selector: 'app-editorial',
     templateUrl: './editorial.component.html',
     styleUrls: ['./editorial.component.scss']
 })
-export class EditorialComponent implements OnInit {
+export class EditorialComponent implements OnInit, OnDestroy {
 
     editing: boolean;
+    saving: boolean;
     content: string;
     new_content: string;
 
-    constructor() { }
+    private _destroyed$ = new Subject();
+
+    constructor(private NCCAPI: NCCAPIService) { }
 
     ngOnInit() {
-        this.content = 'This is some "editorial" text.';
+        this.NCCAPI.getEditorial()
+            .pipe(
+                takeUntil(this._destroyed$)
+            )
+            .subscribe((content: string) => {
+                this.content = content;
+            });
+    }
+
+    ngOnDestroy() {
+        this._destroyed$.next();
     }
 
     beginEdit() {
@@ -23,8 +40,25 @@ export class EditorialComponent implements OnInit {
     }
 
     saveText() {
-        this.content = this.new_content;
-        this.resetText();
+        if (this.saving) {
+            return;
+        }
+
+        this.saving = true;
+
+        const subscription = this.NCCAPI.setEditorial(this.new_content)
+            .subscribe(
+                () => {
+                    console.log('Editorial was updated.');
+                    this.content = this.new_content;
+                    this.resetText();
+                },
+                (error) => { console.log(error); },
+                () => {
+                    this.saving = false;
+                    subscription.unsubscribe();
+                }
+            );
     }
 
     resetText() {
