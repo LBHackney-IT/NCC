d85d2f8a4d46793aa6b89211ea63ce47819bf645
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-10-23 12:52:44 +0100

    More verbose error messages if there was a problem with logging in.

--- ng-ncc-app/src/app/constants/auth.constant.ts
+++ ng-ncc-app/src/app/constants/auth.constant.ts
@@ -1,5 +1,9 @@
 export const AUTH = {
     ROLE_ACCESS: 'Customer service app access',
     ROLE_AGENT: 'NCC Customer Service Representative',
-    ROLE_TEAM_LEADER: 'NCC Team Leader'
+    ROLE_TEAM_LEADER: 'NCC Team Leader',
+
+    FAILED_PERMISSION: 0,
+    FAILED_NO_ACCESS: 1,
+    FAILED_TIMEOUT: 2
 };
