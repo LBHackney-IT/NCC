6a9bee4d9d2761c297a2a3a4ff9e96776b3d188a
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-10-24 14:25:01 +0100

    Adding configurations for development and live "test" sites.

---
+++ ng-ncc-app/src/environments/environment.live-test.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    api: {
+        hackney: 'https://sandboxapi.hackney.gov.uk/hackneyapi',
+        manageATenancy: 'https://sandboxapi.hackney.gov.uk/manageatenancy/v1',
+        ncc: 'https://sandboxapi.hackney.gov.uk/lbhnccapi/api/',
+        notify: 'https://sandboxapi.hackney.gov.uk/lbhnccapi/api/GovNotifier'
+    },
+    anonymousUserID: '9078B253-15C3-E811-A96B-002248072FE8',
+    disable: {
+        previousCalls: true,
+        identifyCaller: true,
+        additionalCallReason: true
+    },
+    previousCallCount: 10
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
