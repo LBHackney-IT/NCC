a65dd33d6a8524b6659f076a54d48fa43831b21c
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-11-07 12:32:24 +0000

    Updating the failed transaciton page.

--- ng-ncc-app/src/app/pages/transaction/failed/failed.component.ts
+++ ng-ncc-app/src/app/pages/transaction/failed/failed.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { PagePaymentMakeComponent } from '../../payment/make/payment-make.component';
 
 @Component({
     selector: 'app-failed',
     templateUrl: './failed.component.html',
     styleUrls: ['./failed.component.scss']
 })
-export class PageTransactionFailedComponent implements OnInit {
-
-    constructor() { }
-
-    ngOnInit() {
-        console.log('AYO');
-    }
-
-}
+export class PageTransactionFailedComponent extends PagePaymentMakeComponent { }
