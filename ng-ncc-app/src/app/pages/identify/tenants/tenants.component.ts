b9a4d02f2d673e2d57af5273d7a49f5ab8c2e7c5
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-11-15 15:06:36 +0000

    Removing console.log()s.

--- ng-ncc-app/src/app/pages/identify/tenants/tenants.component.ts
+++ ng-ncc-app/src/app/pages/identify/tenants/tenants.component.ts
@@ -62,15 +62,12 @@ export class PageIdentifyTenantsComponent implements OnInit {
     tenantSelected(caller: ICaller) {
         if (caller.isAnonymous()) {
             // We've selected "Not a tenant" (a non-tenant caller).
-            console.log('Non-tenant caller.');
-
             // To be able to obtain the respective tenancy reference, we must have a tenant's CRM ID.
             // We'll use the CRM ID of the first tenant in the list.
             const contact_id = this.address.results[0].crmContactId;
             this.Call.setCaller(new NonTenantCaller(contact_id));
         } else {
             // We've identified a tenant as the caller.
-            console.log('Identified caller.');
             this.Call.setCaller(caller);
         }
         this.Call.setTenancy(this.address);
