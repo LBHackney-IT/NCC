bd8e9bd771bf7af7c6e03df7f78084588b6bfb82
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-11-22 11:28:25 +0000

    Created an abstract notes page for common behaviours between View Notes and Add Notes.

--- ng-ncc-app/src/app/pages/add-notes/add-notes.component.ts
+++ ng-ncc-app/src/app/pages/add-notes/add-notes.component.ts
@@ -1,73 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { forkJoin } from 'rxjs';
-import { take, finalize, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 import { PAGES } from '../../constants/pages.constant';
-import { CALL_REASON } from '../../constants/call-reason.constant';
 import { ManageATenancyAPIService } from '../../API/ManageATenancyAPI/manageatenancy-api.service';
-import { NCCAPIService } from '../../API/NCCAPI/ncc-api.service';
 import { NotesService } from '../../services/notes.service';
 import { CallRevisionService } from '../../services/call-revision.service';
-import { PageHistory } from '../abstract/history';
+import { PageNotes } from '../abstract/notes';
 import { BackLinkService } from '../../services/back-link.service';
-import { PageTitleService } from '../../services/page-title.service';
 import { IAccountDetailsByReference } from '../../interfaces/account-details-by-reference';
 import { ILastCall } from '../../interfaces/last-call';
 
 @Component({
     selector: 'app-add-notes',
-    templateUrl: './add-notes.component.html',
-    styleUrls: ['./add-notes.component.scss']
+    templateUrl: '../view-notes/view-notes.component.html',
+    styleUrls: ['../view-notes/view-notes.component.scss']
 })
-export class PageAddNotesComponent extends PageHistory implements OnInit {
+export class PageAddNotesComponent extends PageNotes implements OnInit {
     // This page is similar to the View Notes page, except it doesn't have a dependency on the Call service or an identified caller.
-    // TODO perhaps we could reuse the View Notes page.
 
     previous_call: ILastCall;
-    tenants_list: string[];
 
-    // NOTES FILTER SETTINGS.
-    filter_settings: {
-        min_date: Date,
-        max_date: Date,
-        manual: { [propKey: string]: string }
-    };
+    BackLink: BackLinkService;
+    CallRevision: CallRevisionService;
+    ManageATenancyAPI: ManageATenancyAPIService;
+    Notes: NotesService;
+    router: Router;
 
-    filter_reason: string;
-    filter_tenant: string;
+    constructor(private injectorObj: Injector) {
+        super(injectorObj);
+        this.BackLink = this.injectorObj.get(BackLinkService);
+        this.CallRevision = this.injectorObj.get(CallRevisionService);
+        this.ManageATenancyAPI = this.injectorObj.get(ManageATenancyAPIService);
+        this.Notes = this.injectorObj.get(NotesService);
+        this.router = this.injectorObj.get(Router);
+    }
 
-    constructor(
-        private router: Router,
-        private BackLink: BackLinkService,
-        private ManageATenancyAPI: ManageATenancyAPIService,
-        private NCCAPI: NCCAPIService,
-        private CallRevision: CallRevisionService,
-        private PageTitle: PageTitleService,
-        private Notes: NotesService
-    ) {
-        super();
-        this.previous_call = this.CallRevision.getPreviousCall();
-
+    ngOnInit() {
         // Are we meant to be on this page?
+        this.previous_call = this.CallRevision.getPreviousCall();
         if (!this.previous_call) {
             this.router.navigate([PAGES.PREVIOUS_CALLS.route]);
             return false;
         }
-    }
 
-    ngOnInit() {
+        super.ngOnInit();
         this.PageTitle.set(PAGES.VIEW_NOTES.label);
 
         // Enable the back link and have it go to the "home" page.
         this.BackLink.enable();
         this.BackLink.setTarget(PAGES.PREVIOUS_CALLS.route);
-
-        // Set up the filter.
-        this.clearFilter();
-
-        // Generate a list of tenants.
-        this.getTenants();
 
         // Display the add note form.
         const settings = {
@@ -93,36 +76,12 @@ export class PageAddNotesComponent extends PageHistory implements OnInit {
     /**
      * Fetches a list of tenants associated with the specified tenancy reference.
      */
-    getTenants() {
-        this.ManageATenancyAPI.getAccountDetailsByReference(this.getTenancyReference())
+    getTenants(): Observable<string[]> {
+        return this.ManageATenancyAPI.getAccountDetailsByReference(this.getTenancyReference())
             .pipe(take(1))
-            .subscribe((result: IAccountDetailsByReference) => {
-                this.tenants_list = result.ListOfTenants.map((tenant) => `${tenant.forename} ${tenant.surname}`);
-            });
-    }
-
-    /**
-     * Set the manual filtering options for the list of notes.
-     */
-    filterNotes() {
-        this.filter.manual = {
-            callReasonType: this.filter_reason,
-            clientName: this.filter_tenant
-        };
-    }
-
-    /**
-     * Clears/resets the notes filter settings.
-     */
-    clearFilter() {
-        this.filter = {
-            min_date: null,
-            max_date: null,
-            manual: {}
-        };
-        this.filter_reason = null;
-        this.filter_tenant = null;
-        this.filterNotes();
+            .pipe(map((result: IAccountDetailsByReference) => {
+                return result.ListOfTenants.map((tenant) => `${tenant.forename} ${tenant.surname}`);
+            }));
     }
 
 }
--- ng-ncc-app/src/app/pages/view-notes/view-notes.component.ts
+++ ng-ncc-app/src/app/pages/view-notes/view-notes.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, Injector, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { PAGES } from '../../constants/pages.constant';
-import { PageHistory } from '../abstract/history';
+import { PageNotes } from '../abstract/notes';
 import { IdentifiedCaller } from '../../classes/identified-caller.class';
 import { CallService } from '../../services/call.service';
 import { PageTitleService } from '../../services/page-title.service';
@@ -14,75 +14,37 @@ import { PageTitleService } from '../../services/page-title.service';
     templateUrl: './view-notes.component.html',
     styleUrls: ['./view-notes.component.scss']
 })
-export class PageViewNotesComponent extends PageHistory implements OnInit, OnDestroy {
-
-    private _destroyed$ = new Subject();
+export class PageViewNotesComponent extends PageNotes implements OnInit {
 
     caller: IdentifiedCaller;
-    tenants: { [propKey: string]: string }[];
 
-    filter_settings: {
-        min_date: Date,
-        max_date: Date,
-        manual: { [propKey: string]: string }
-    };
-
-    filter_reason: string;
-    filter_caller: string;
-
-    constructor(private route: ActivatedRoute, private Call: CallService, private PageTitle: PageTitleService) {
-        super();
+    constructor(private injectorObj: Injector, private Call: CallService) {
+        super(injectorObj);
     }
 
     /**
      *
      */
     ngOnInit() {
+        super.ngOnInit();
         this.PageTitle.set(PAGES.VIEW_NOTES.label);
-
-        // Set up the filter.
-        this.filter = {
-            min_date: null,
-            max_date: null,
-            manual: {}
-        };
-        this.tenants = this.Call.getTenants();
-        this.clearFilter();
     }
 
-    ngOnDestroy() {
-        this._destroyed$.next();
+    /**
+     * Fetches a list of tenant names associated with the specified tenancy reference.
+     */
+    getTenants(): Observable<string[]> {
+        const tenants = this.Call.getTenants();
+        const tenant_names = tenants.map((tenant) => tenant.full_name);
+
+        return of(tenant_names);
     }
 
+    /**
+     * Returns the tenancy reference to fetch notes for.
+     */
     getTenancyReference(): string {
         return this.Call.getTenancyReference();
-    }
-
-    /**
-     * Set the manual filtering options for the list of transactions.
-     */
-    filterTransactions() {
-        this.filter.manual = {
-            callReasonType: this.filter_reason,
-            clientName: this.filter_caller
-        };
-    }
-
-    /**
-     *
-     */
-    clearFilter() {
-        this.filter_reason = null;
-        this.filter_caller = null;
-        this.filterTransactions();
-    }
-
-    /**
-     *
-     */
-    trackByTenants(index: number, item: { [propKey: string]: string }): string {
-        // TODO: item should use an interface.
-        return item.contact_id;
     }
 
 }
