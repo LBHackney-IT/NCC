b2e1a0232b9b15dbbf253ff25df1640da007caf1
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-11-29 11:37:04 +0000

    Added a script to provide the project's version details.

---
+++ ng-ncc-app/src/environments/version.ts
@@ -0,0 +1,14 @@
+// DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "19f9f5e-dirty",
+    "hash": "19f9f5e",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "19f9f5e-dirty",
+    "semverString": null,
+    "version": "0.0.0"
+};
+/* tslint:enable */
