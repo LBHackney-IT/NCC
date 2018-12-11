beaec1d039f358567a55b0cc3203072f4d4e3f77
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-12-11 15:02:39 +0000

    Corrections as a result of linting.

--- ng-ncc-app/src/app/directives/numbers-only.directive.ts
+++ ng-ncc-app/src/app/directives/numbers-only.directive.ts
@@ -12,13 +12,13 @@ export class NumbersOnlyDirective {
 
     @HostListener('keydown', ['$event'])
     onKeyDown(event: KeyboardEvent) {
-        let next: string = this.el.nativeElement.value.concat(event.key);
+        const next: string = this.el.nativeElement.value.concat(event.key);
         this._update(next);
     }
 
     @HostListener('paste', ['$event'])
     onChange(event: KeyboardEvent) {
-        let next: string = this.el.nativeElement.value;
+        const next: string = this.el.nativeElement.value;
         this._update(next);
     }
 
