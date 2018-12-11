f7bb76a524c1c3c1439efc5ef87bae76609f88e2
Drew Maughan <drew.maughan@hackney.gov.uk>
2018-12-11 15:01:14 +0000

    Preventing pasting non-numeric characters into numeric fields.

--- ng-ncc-app/src/app/directives/numbers-only.directive.ts
+++ ng-ncc-app/src/app/directives/numbers-only.directive.ts
@@ -13,7 +13,16 @@ export class NumbersOnlyDirective {
     @HostListener('keydown', ['$event'])
     onKeyDown(event: KeyboardEvent) {
         let next: string = this.el.nativeElement.value.concat(event.key);
+        this._update(next);
+    }
 
+    @HostListener('paste', ['$event'])
+    onChange(event: KeyboardEvent) {
+        let next: string = this.el.nativeElement.value;
+        this._update(next);
+    }
+
+    private _update(next: string) {
         if (next) {
             next = next.replace(this.regex, '');
         }
@@ -25,5 +34,4 @@ export class NumbersOnlyDirective {
             }
         }, 1);
     }
-
 }
