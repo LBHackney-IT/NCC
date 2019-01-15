44fdc4978a85ba96be71152069a58f63d084079d
Drew Maughan <drew.maughan@hackney.gov.uk>
2019-01-15 11:40:23 +0000

    Version 5.9.

--- ng-ncc-app/src/environments/version.ts
+++ ng-ncc-app/src/environments/version.ts
@@ -1,14 +1,14 @@
 // DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "83327bf",
-    "hash": "83327bf",
+    "dirty": true,
+    "raw": "9e4e0b7-dirty",
+    "hash": "9e4e0b7",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "83327bf",
+    "suffix": "9e4e0b7-dirty",
     "semverString": null,
-    "version": "5.8.0"
+    "version": "5.9.0"
 };
 /* tslint:enable */
