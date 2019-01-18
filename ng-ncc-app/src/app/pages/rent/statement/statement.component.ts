2761af224c2e40fa6b70c7c7572a156ccd00699b
Drew Maughan <drew.maughan@hackney.gov.uk>
2019-01-18 14:06:00 +0000

    Use the updated information from GetTenancyAgreementDetails.

--- ng-ncc-app/src/app/pages/rent/statement/statement.component.ts
+++ ng-ncc-app/src/app/pages/rent/statement/statement.component.ts
@@ -12,10 +12,12 @@ import { PAGES } from '../../../constants/pages.constant';
 import { CONTACT } from '../../../constants/contact.constant';
 import { PageCommunications } from '../../abstract/communications';
 import { BackLinkService } from '../../../services/back-link.service';
+import { NCCAPIService } from '../../../API/NCCAPI/ncc-api.service';
 import { CommsSelection } from '../../../classes/comms-selection.class';
 import { IAccountDetails } from '../../../interfaces/account-details';
 import { INotifyAPIJSONResult } from '../../../interfaces/notify-api-json-result';
 import { INotifyStatementParameters } from '../../../interfaces/notify-statement-parameters';
+import { ITenancyAgreementDetails } from '../../../interfaces/tenancy-agreement-details';
 import { UHTriggerService } from '../../../services/uhtrigger.service';
 
 @Component({
@@ -25,7 +27,8 @@ import { UHTriggerService } from '../../../services/uhtrigger.service';
 })
 export class PageRentStatementComponent extends PageCommunications implements OnInit {
 
-    account: IAccountDetails;
+    tenancyDetails: ITenancyAgreementDetails;
+    tenancyReference: string;
     from_date: string;
     until_date: string;
     statement_url: SafeResourceUrl;
@@ -33,6 +36,7 @@ export class PageRentStatementComponent extends PageCommunications implements On
     success_message: string;
 
     private BackLink: BackLinkService;
+    private NCCAPI: NCCAPIService;
     private sanitiser: DomSanitizer;
 
     constructor(@Inject(LOCALE_ID) private locale: string, private injector: Injector) {
@@ -40,6 +44,7 @@ export class PageRentStatementComponent extends PageCommunications implements On
         this.BackLink = this.injector.get<BackLinkService>(BackLinkService);
         this.sanitiser = this.injector.get(DomSanitizer);
         this.UHTrigger = this.injector.get<UHTriggerService>(UHTriggerService);
+        this.NCCAPI = this.injector.get<NCCAPIService>(NCCAPIService);
         // see https://stackoverflow.com/questions/49424837/lint-warning-get-is-deprecated-when-trying-to-manually-inject-ngcontrol
     }
 
@@ -53,11 +58,12 @@ export class PageRentStatementComponent extends PageCommunications implements On
         this.until_date = moment().format('DD/MM/YYYY');
         this.from_date = moment().subtract(30, 'days').format('DD/MM/YYYY');
 
-        // Obtain account details.
-        this.Call.getAccount()
+        // Obtain tenancy agreeement details.
+        this.tenancyReference = this.Call.getTenancyReference();
+        this.NCCAPI.getTenancyAgreementDetails(this.tenancyReference)
             .pipe(takeUntil(this._destroyed$))
-            .subscribe((account) => {
-                this.account = account;
+            .subscribe((details: ITenancyAgreementDetails) => {
+                this.tenancyDetails = details;
 
                 // Automatically generate a statement.
                 this.refreshStatement();
@@ -97,7 +103,7 @@ export class PageRentStatementComponent extends PageCommunications implements On
     refreshStatement() {
         if (this.canRefresh()) {
             const url = `${environment.api.statement}?contactid=${this.getContactID()}` +
-                `&tenagreementref=${this.account.tagReferenceNumber}` +
+                `&tenagreementref=${this.tenancyReference}` +
                 `&startdate=${this.from_date}` +
                 `&enddate=${this.until_date}`;
             // console.log(url);
@@ -146,14 +152,14 @@ export class PageRentStatementComponent extends PageCommunications implements On
                 // Build a set of parameters for sending the statement.
                 const parameters = {
                     EmailTo: this.selected_details.getDetail(),
-                    TenancyReference: this.account.tagReferenceNumber,
+                    TenancyReference: this.tenancyReference,
                     ContactId: this.getContactID(),
                     StartDate: this.from_date,
                     EndDate: this.until_date,
                     TemplateId: environment.notifyTemplate.statement,
                     TemplateData: {
-                        'rent amount': formatCurrency(this.account.rent, this.locale, '£'),
-                        'rent balance': formatCurrency(this.account.currentBalance, this.locale, '£')
+                        'rent amount': formatCurrency(this.tenancyDetails.rent, this.locale, '£'),
+                        'rent balance': formatCurrency(this.tenancyDetails.currentBalance, this.locale, '£')
                     }
                 } as INotifyStatementParameters;
 
--- ng-ncc-app/src/environments/version.ts
+++ ng-ncc-app/src/environments/version.ts
@@ -2,13 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "9e4e0b7-dirty",
-    "hash": "9e4e0b7",
+    "raw": "315cab9-dirty",
+    "hash": "315cab9",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "9e4e0b7-dirty",
+    "suffix": "315cab9-dirty",
     "semverString": null,
-    "version": "5.9.0"
+    "version": "5.10.0"
 };
 /* tslint:enable */
