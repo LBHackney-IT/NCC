e2a84cca6ae7d2988a2aeea13a9a687dab6a7d4b
Drew Maughan <drew.maughan@hackney.gov.uk>
2019-01-22 16:48:26 +0000

    Use the display balance in rent comms templates.

--- ng-ncc-app/src/app/pages/rent/communications/communications.component.ts
+++ ng-ncc-app/src/app/pages/rent/communications/communications.component.ts
@@ -7,9 +7,10 @@ import { PAGES } from '../../../constants/pages.constant';
 import { CommsOption } from '../../../classes/comms-option.class';
 import { PageCommunications } from '../../abstract/communications';
 import { DPAService } from '../../../services/dpa.service';
-import { NextPaymentService } from '../../../services/next-payment.service';
+import { NCCAPIService } from '../../../API/NCCAPI/ncc-api.service';
 import { IAccountDetails } from '../../../interfaces/account-details';
 import { IAddressSearchGroupedResult } from '../../../interfaces/address-search-grouped-result';
+import { ITenancyAgreementDetails } from '../../../interfaces/tenancy-agreement-details';
 
 @Component({
     selector: 'app-rent-communications',
@@ -18,14 +19,15 @@ import { IAddressSearchGroupedResult } from '../../../interfaces/address-search-
 })
 export class PageRentCommunicationsComponent extends PageCommunications implements OnInit {
 
-    NextPayment: NextPaymentService;
+    NCCAPI: NCCAPIService;
     tenancy: IAddressSearchGroupedResult;
+    tenancyDetails: ITenancyAgreementDetails;
     is_non_tenant: boolean;
     is_identified: boolean;
 
     constructor(@Inject(LOCALE_ID) private locale: string, private injector: Injector) {
         super(injector);
-        this.NextPayment = this.injector.get(NextPaymentService);
+        this.NCCAPI = this.injector.get(NCCAPIService);
     }
 
     /**
@@ -40,6 +42,17 @@ export class PageRentCommunicationsComponent extends PageCommunications implemen
         this.Call.getAccount()
             .pipe(takeUntil(this._destroyed$))
             .subscribe((account: IAccountDetails) => { this.account_details = account; });
+
+        // Obtain tenancy agreeement details.
+        this.tenancyReference = this.Call.getTenancyReference();
+        this.NCCAPI.getTenancyAgreementDetails(this.tenancyReference)
+            .pipe(takeUntil(this._destroyed$))
+            .subscribe((details: ITenancyAgreementDetails) => {
+                this.tenancyDetails = details;
+
+                // Automatically generate a statement.
+                this.refreshStatement();
+            });
     }
 
     /**
@@ -77,10 +90,9 @@ export class PageRentCommunicationsComponent extends PageCommunications implemen
     updatePreview() {
         super.updatePreview();
         if (this.isSensitiveTemplateSelected() && this.preview) {
-            const next_payment = this.NextPayment.calculate(this.account_details);
             this.preview.parameters = {
                 'Tenancy reference': this.account_details.tagReferenceNumber,
-                'Rent balance': formatCurrency(next_payment, this.locale, '£'),
+                'Rent balance': formatCurrency(this.tenancyDetails.displayBalance, this.locale, '£'),
                 'Rent amount': formatCurrency(this.account_details.rent, this.locale, '£')
             };
         }
