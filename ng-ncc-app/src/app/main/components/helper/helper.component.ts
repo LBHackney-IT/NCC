469c31650d1bc6225b102fa661efd5f061c57a79
Drew <drew.maughan@hackney.gov.uk>
2019-02-21 11:51:55 +0000

    The ability to configure a helper to display the tip below the icon.

--- ng-ncc-app/src/app/main/components/helper/helper.component.ts
+++ ng-ncc-app/src/app/main/components/helper/helper.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
     selector: 'app-helper',
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./helper.component.scss']
 })
 export class HelperComponent {
+
+    @HostBinding('class.helper--below')
+    @Input() below: boolean;
 }
