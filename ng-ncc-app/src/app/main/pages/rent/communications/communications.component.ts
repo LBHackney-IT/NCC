55c7cf195bd9d223e41ade7f915b15fb48cf5ec3
Drew <drew.maughan@hackney.gov.uk>
2019-02-22 15:22:50 +0000

    Adding a service [charge] amount to sensitive comms.

--- ng-ncc-app/src/app/main/pages/rent/communications/communications.component.ts
+++ ng-ncc-app/src/app/main/pages/rent/communications/communications.component.ts
@@ -1,16 +1,15 @@
 import { LOCALE_ID, Component, Inject, Injector, OnInit } from '@angular/core';
 import { formatCurrency } from '@angular/common';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, concatMap } from 'rxjs/operators';
 
 import { PAGES } from '../../../../common/constants/pages.constant';
 import { CommsOption } from '../../../../common/classes/comms-option.class';
 import { PageCommunications } from '../../abstract/communications';
-import { DPAService } from '../../../../common/services/dpa.service';
 import { NCCAPIService } from '../../../../common/API/NCCAPI/ncc-api.service';
 import { IAccountDetails } from '../../../../common/interfaces/account-details';
 import { IAddressSearchGroupedResult } from '../../../../common/interfaces/address-search-grouped-result';
 import { ITenancyAgreementDetails } from '../../../../common/interfaces/tenancy-agreement-details';
+import { IRentBreakdown } from '../../../../common/interfaces/rent-breakdown';
 
 @Component({
     selector: 'app-rent-communications',
@@ -24,6 +23,7 @@ export class PageRentCommunicationsComponent extends PageCommunications implemen
     tenancyDetails: ITenancyAgreementDetails;
     is_non_tenant: boolean;
     is_identified: boolean;
+    accountBreakdown: IRentBreakdown[];
 
     constructor(@Inject(LOCALE_ID) private locale: string, private injector: Injector) {
         super(injector);
@@ -39,9 +39,18 @@ export class PageRentCommunicationsComponent extends PageCommunications implemen
         this.tenancy = this.Call.getTenancy();
         this.is_non_tenant = this.Call.isCallerNonTenant();
         this.is_identified = this.Call.isCallerIdentified();
+
+        // Obtain the account details.
         this.Call.getAccount()
             .pipe(takeUntil(this._destroyed$))
-            .subscribe((account: IAccountDetails) => { this.account_details = account; });
+            .pipe(concatMap((account: IAccountDetails) => {
+                this.account_details = account;
+                return this.NCCAPI.getRentBreakdown(account.tagReferenceNumber);
+            }))
+            .subscribe((data: IRentBreakdown[]) => {
+                this.accountBreakdown = data;
+                console.log('Account breakdown:', data);
+            });
 
         // Obtain tenancy agreeement details.
         this.NCCAPI.getTenancyAgreementDetails(this.account_details.tagReferenceNumber)
@@ -81,7 +90,7 @@ export class PageRentCommunicationsComponent extends PageCommunications implemen
     }
 
     /**
-     *
+     * Update the comms template preview with specific values for template paramteters.
      */
     updatePreview() {
         super.updatePreview();
@@ -89,9 +98,26 @@ export class PageRentCommunicationsComponent extends PageCommunications implemen
             this.preview.parameters = {
                 'Tenancy reference': this.account_details.tagReferenceNumber,
                 'Rent balance': formatCurrency(this.tenancyDetails.displayBalance, this.locale, '£'),
-                'Rent amount': formatCurrency(this.account_details.rent, this.locale, '£')
+                'Rent amount': formatCurrency(this.account_details.rent, this.locale, '£'),
+                'Service balance': formatCurrency(this.tenancyDetails.displayBalance, this.locale, '£'),
+                'Service amount': formatCurrency(this._accountServiceCharge, this.locale, '£'),
             };
         }
+    }
+
+    /**
+     * Returns the service charge for the account, if present.
+     */
+    private get _accountServiceCharge() {
+        let row: IRentBreakdown;
+        if (this.accountBreakdown) {
+            row = this.accountBreakdown.find((row: IRentBreakdown) => 'DSC' === row.code);
+            // TODO we will probably want a constants file containing all possible rent breakdown codes.
+        }
+
+        console.log('Service charge is:', row);
+
+        return row ? row.value : null;
     }
 
     /**
